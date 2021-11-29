f1240618a5eafd366493a878f98a67c8f9d577cb
Hamza Bouyahya <hamzabouyahya198@gmail.com>
2021-11-29 12:18:40 +0100

    first commit

---
+++ src/Components/CenteredTitle/CenteredTitle.js
@@ -0,0 +1,20 @@
+// import { Button } from '@mui/material';
+import React from 'react'
+import './CenteredTitle.scss';
+
+function CenteredTitle() {
+
+
+    return (
+        <div id="text_div center_all">
+            <div className='center_all'>
+                <h1 className='custom-subTitle'>
+                    Nasa Definition
+                </h1>
+                <button className='btn_outlined'>Make the Difference</button>
+            </div>
+        </div>
+    )
+}
+
+export default CenteredTitle
